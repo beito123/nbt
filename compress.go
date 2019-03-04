009650e35ec50b172000e19e6e0aa19e09ae44e7
beito123 <mattcha@gmx.com>
2019-03-04 23:23:30 +0900

    Support zlib compression

--- compress.go
+++ compress.go
@@ -12,6 +12,7 @@ package nbt
 import (
 	"bytes"
 	"compress/gzip"
+	"compress/zlib"
 	"io"
 	"io/ioutil"
 	"os"
@@ -19,11 +20,31 @@ import (
 	"github.com/beito123/binary"
 )
 
+// CompressType is a compression type
+type CompressType int
+
+const (
+	// CompressGZip compresses with gzip
+	CompressGZip CompressType = iota
+
+	// CompressZlib compresses with zlib
+	CompressZlib
+)
+
+// Compression Detector
+
 var gzipHeader = []byte{0x1f, 0x8b, 0x08}
+var zlibHeader = []byte{0x78}
 
 func hasGZipHeader(b []byte) bool {
 	return bytes.HasPrefix(b, gzipHeader)
 }
+
+func hasZlibHeader(b []byte) bool {
+	return bytes.HasPrefix(b, zlibHeader)
+}
+
+//
 
 // FromFile returns new stream from file
 // If the data is comressed, uncompresses with gzip
@@ -37,7 +58,7 @@ func FromFile(path string, order binary.Order) (*Stream, error) {
 }
 
 // FromReader returns new stream from Reader
-// If the data is compressed, uncompresses with gzip
+// If the data is compressed, uncompresses
 func FromReader(reader io.Reader, order binary.Order) (*Stream, error) {
 	b, err := ioutil.ReadAll(reader)
 	if err != nil {
@@ -56,25 +77,57 @@ func FromReader(reader io.Reader, order binary.Order) (*Stream, error) {
 		if err != nil {
 			return nil, err
 		}
+	} else if hasZlibHeader(b) {
+		read, err := zlib.NewReader(bytes.NewBuffer(b))
+		if err != nil {
+			return nil, err
+		}
+
+		defer read.Close()
+
+		b, err = ioutil.ReadAll(read)
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	return NewStreamBytes(order, b), nil
 }
 
-// Compress compresses Stream's bytes
+// Compress compresses stream's bytes
+// You can use compression level in "compress/gzip" and "compress/zlib" for level
 // This often is used for player and level data
-func Compress(s *Stream) ([]byte, error) {
+func Compress(s *Stream, typ CompressType, level int) ([]byte, error) {
 	buf := bytes.NewBuffer([]byte{})
 
-	writer := gzip.NewWriter(buf)
+	switch typ {
+	case CompressGZip:
+		writer, err := gzip.NewWriterLevel(buf, level)
+		if err != nil {
+			return nil, err
+		}
 
-	_, err := writer.Write(s.Bytes())
-	if err != nil {
-		return nil, err
+		_, err = writer.Write(s.Bytes())
+		if err != nil {
+			return nil, err
+		}
+
+		writer.Flush()
+		writer.Close()
+	case CompressZlib:
+		writer, err := zlib.NewWriterLevel(buf, level)
+		if err != nil {
+			return nil, err
+		}
+
+		_, err = writer.Write(s.Bytes())
+		if err != nil {
+			return nil, err
+		}
+
+		writer.Flush()
+		writer.Close()
 	}
-
-	writer.Flush()
-	writer.Close()
 
 	return buf.Bytes(), nil
 }
